cb034f0f2831c908c370cd4b9c1c9c674b33b6e4
David Yi <David Yi>
2016-04-03 11:52:19 -0400

    my proj

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# HackBCAIII
-My project for HackBCAIII
+flask-boilerplate
+=================
+
+Ready to use and deploy to Heroku!
+
+[![Deploy](https://www.herokucdn.com/deploy/button.png)](https://heroku.com/deploy)
---
+++ app.py
@@ -0,0 +1,106 @@
+from flask import Flask, render_template, request, redirect
+from twilio.rest import TwilioRestClient
+from twilio import twiml
+from twilio.rest.lookups import TwilioLookupsClient
+from microsofttranslator import Translator
+import jinja2
+import os
+
+app = Flask(__name__)
+ACCOUNT_SID = "AC5710ab4dd464377fc2a0f14704dc25c0" 
+AUTH_TOKEN = "616dcf9e1f3aeecce709127c5bac00ee" 
+MY_CLIENT_ID = 'hackbca3'
+MY_CLIENT_SECRET = 'AwENjKefBWchBJL5hIS0hOQfQmlOQY4Y1BY+Sg7HuPA='
+
+
+client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN)
+translator = Translator(MY_CLIENT_ID, MY_CLIENT_SECRET)
+conversations = {}
+languageSpoken = {}
+supported_languages = {'ar' : ' Arabic','bg' : 'Bulgarian','ca' : 'Catalan','zh-CHS' : 'Chinese (Simplified)',
+	'zh-CHT' : 'Chinese (Traditional)','cs' : 'Czech','da' : 'Danish','nl' : 'Dutch','en' : 'English','et' : 'Estonian',
+	'fi' : 'Finnish','fr' : 'French','de' : 'German','el' : 'Greek','ht' : 'Haitian Creole','he' : 'Hebrew','hi' : 'Hindi',
+	'hu' : 'Hungarian','id' : 'Indonesian','it' : 'Italian','ja' : 'Japanese','ko' : 'Korean','lv' : 'Latvian','lt' : 'Lithuanian',
+	'mww' : 'Hmong Daw','no' : 'Norwegian','pl' : 'Polish','pt' : 'Portuguese','ro' : 'Romanian','ru' : 'Russian','sk' : 'Slovak','sl' : 'Slovenian',
+	'es' : 'Spanish','sv' : 'Swedish','th' : 'Thai','tr' : 'Turkish','uk' : 'Ukrainian','vi' : 'Vietnamese',
+}
+supported_languages = {y.lower():x for x,y in supported_languages.iteritems()}
+
+twilioPhone = "15519996857"
+
+#recieve the text message
+@app.route("/", methods=['GET', 'POST'])
+def receive():
+	message = request.form["Body"]
+	sender = request.form["From"]
+	language = None
+	recipient = None
+	resp = twiml.Response()
+	
+	if message == "\help":
+		message = "\"@@[phone number]\" - choose the recipient of the translated message\n" \
+			"\"@+[language]\" - choose the language that you want it to be translated to\n" \
+			"\"\lang\" - to see the supported languages\n" \
+			"Just send any message in proper english and I will translate it to your wanted" \
+			" language and send it to the recipient."
+		resp.message(message)
+		return str(resp)
+	elif message == "\lang":
+		message = ""
+		for x in supported_languages:
+			message += x +': ' + supported_languages[x] + "\n"
+		resp.message(message)
+		return str(resp)
+	elif message[0:2] == "@@":
+		try:
+			int(message[2:])
+			recipient = message[2:]
+			conversations[sender] = recipient
+			conversations[recipient] = sender
+		except ValueError:
+			message = message[2:] + " is not a valid phone number. Please submit a new " \
+			"and valid phone number beginning with \"@@\". If you need help " \
+			"enter \"\help\""
+			resp.message(message)
+			return str(resp)
+	elif message[0:2] == "@+":
+		language = message[2:].lower()
+		if language not in supported_languages.values():
+			message = language + " is not a valid/and or supported language. Please submit a " \
+			"new and valid phone number beginning with \"@@\". If you need help " \
+			"enter \"\help\""
+			resp.message(message)
+			return str(resp)
+		else:
+#			languageSpoken[conversations[sender]] = language
+#			languageSpoken[sender] = translator.detect_language(message)
+			message = "The language to be translated has changed to "
+			for lang, code in supported_languages.iteritems():
+				if code == language:
+					message += lang
+			resp.message(message)
+			return str(resp)
+	else:
+		#if (conversations[sender] is None):
+		#	message = "Please input a recipient to translate the message."
+		#	resp.message(message)
+		#	return str(resp)
+		#else:
+		#if (languageSpoken[conversations[sender]] is None):
+		#	language = languageSpoken[sender] #default lang is the same as the recipient
+		#message = translator.TransformTextMethod(message)
+		translate(sender, language, message)
+
+
+    
+def translate(sender, language, message):
+	message = translator.translate(message, language)
+	send(sender, message)
+
+def send(sender, message, reci):
+	message = client.messages.create(to=conversations[sender], from_=twilioPhone,
+                                     body = message)
+
+if __name__ == '__main__':
+	port = int(os.environ.get('PORT', 8000))
+	app.run(host='0.0.0.0', port=port,debug=True)
